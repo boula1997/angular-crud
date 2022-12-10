cf8db520cc655c572b19405db6400f948a071d97
boula1997 <boula.nessim.soliman@gmail.com>
2022-12-10 02:00:48 +0200

    uploading project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FruitsRoutingModule } from './fruits/fruits-routing.module';
+import { FruitsModule } from './fruits/fruits.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FruitsRoutingModule,
+    FruitsModule,
+    HttpClientModule
+    
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
